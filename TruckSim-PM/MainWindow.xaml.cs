90eb3b7be3240a8425ba714ecdbf1379475ca11f
playhaux <esportscrawler@gmail.com>
2024-04-13 18:13:19 +0200

    Add datagridview binding

--- TruckSim-PM/MainWindow.xaml.cs
+++ TruckSim-PM/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -28,7 +29,7 @@ namespace TruckSim_PM
             { 
                 throw new NotImplementedException();
             }
-
+            dgProfiles.ItemsSource = playerprofiles;
         }
     }
 }
