30299f067255cc0b0e1454e14ff71b36c39d3b1e
playhaux <esportscrawler@gmail.com>
2024-04-15 11:14:40 +0200

    Change timestamp string in file name

--- TruckSim-PM/MainWindow.xaml.cs
+++ TruckSim-PM/MainWindow.xaml.cs
@@ -155,7 +155,7 @@ namespace TruckSim_PM
             // Configure save file dialog box
             SaveFileDialog saveFileDialog = new SaveFileDialog
             {
-                FileName = string.Format("{0}-{1}.zip", profile.DirectoryShort, DateTime.Now.ToString("yyyy-dd-M--HH-mm-ss")),
+                FileName = string.Format("{0}-{1}.zip", profile.DirectoryShort, DateTime.Now.ToString("yyyy-dd-M_HH-mm-ss")),
                 DefaultExt = ".zip", // Default file extension
                 Filter = "Zip files (.zip)|*.zip", // Filter files by extension
                 Title = "Select directory and file name",
